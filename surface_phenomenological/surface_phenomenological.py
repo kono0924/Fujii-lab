16382e9a7e8cfe0bb9440dea0be2efb7369ba8d8
kono0924 <konofr2711@gmail.com>
2022-07-29 13:44:37 +0900

    20220729

--- surface_phenomenological/surface_phenomenological.py
+++ surface_phenomenological/surface_phenomenological.py
@@ -390,17 +390,17 @@ def count(trials,code_distance,p_div,pm,result_list):
 if __name__ == "__main__":
 
     ### パラメータ
-    trials = 3000
-    p_s = 0.5
-    p_e = 2
-    p_d = 0.25
-    d_s = 5
-    d_e = 9
+    trials = 100
+    p_s = 6
+    p_e = 12
+    p_d = 1
+    d_s = 3
+    d_e = 7
     d_d = 2
     pro = 100
     code_distance = np.arange(d_s,d_e+1,d_d)
     p_div = np.arange(p_s,p_e+p_d,p_d)
-    pm = 2.5
+    pm = 0
 
     # プロセスを管理する人。デラックスな共有メモリ
     manager = multiprocessing.Manager()
