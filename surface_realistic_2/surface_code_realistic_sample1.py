83d33f01e1123acb123200e4dc469c27043935cf
kono0924 <konofr2711@gmail.com>
2022-10-17 19:02:18 +0900

    221017

--- surface_realistic_2/surface_code_realistic_sample1.py
+++ surface_realistic_2/surface_code_realistic_sample1.py
@@ -699,7 +699,7 @@ def count(trials,cd_sur_list,p_list,eta,cd_rep,result_list):
 if __name__ == "__main__":
 
     ### パラメータ
-    trials = 200
+    trials = 20
     d_s = 3
     d_e = 9
     d_d = 2
