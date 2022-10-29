331ebd13b8a5751c2817137a9387f547f3b13af4
kono0924 <konofr2711@gmail.com>
2022-10-29 19:23:26 +0900

    221029

--- surface_realistic/realistic2/【realistic2】.py
+++ surface_realistic/realistic2/【realistic2】.py
@@ -803,7 +803,7 @@ def count(trials,cd_sur_list,p_list,eta,cd_rep,round_rep,result_list):
 if __name__ == "__main__":
 
     ### パラメータ
-    trials = 1
+    trials = 20
     d_s = 3
     d_e = 9
     d_d = 2
@@ -815,7 +815,7 @@ if __name__ == "__main__":
     round_rep = 10
     p_list = np.arange(p_s,p_e+p_d,p_d)
     cd_sur_list = np.arange(d_s,d_e+1,d_d)
-    pro = 1
+    pro = 500
 
     # プロセスを管理する人。デラックスな共有メモリ
     manager = multiprocessing.Manager()
