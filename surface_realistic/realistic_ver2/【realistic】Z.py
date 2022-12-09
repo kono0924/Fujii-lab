d73440df1091e8d4606583505debe567ae455228
河野秀城 <konohideki@kounoshuujounoMacBook-Air.local>
2022-12-09 20:23:43 +0900

    221209

--- surface_realistic/realistic_ver2/【realistic】Z.py
+++ surface_realistic/realistic_ver2/【realistic】Z.py
@@ -937,8 +937,8 @@ if __name__ == "__main__":
     p_e = 0.008
     p_d = 0.001
     eta = 1000
-    cd_rep = 11
-    round_rep = 5
+    cd_rep = 7
+    round_rep = 10
     round_sur = 11
     p_list = np.arange(p_s,p_e+p_d,p_d)
     cd_sur_list = np.arange(d_s,d_e+1,d_d)
